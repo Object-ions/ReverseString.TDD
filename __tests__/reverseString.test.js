c6ff64bf4301875a4f21bfd88388750e92acb12b
object-ions <moshikoatia@gmail.com>
2023-11-08 13:38:03 -0800

    Write 3rd test and pass - should return reversed string

--- __tests__/reverseString.test.js
+++ __tests__/reverseString.test.js
@@ -4,8 +4,12 @@ describe('reverse string', () => {
   it('should be a function', () => {
     expect(typeof reverseString).toEqual('function');
   });
-  
+
   it('should return a string', () => {
     expect(typeof reverseString('hello')).toEqual('string');
+  });
+
+  it('should return the reversed string', () => {
+    expect(reverseString('hello')).toEqual('olleh');
   });
 });
