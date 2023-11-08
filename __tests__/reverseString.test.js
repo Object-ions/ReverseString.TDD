1db6ff6f090cef6d8f369ca1ebf5811a28fa6a05
object-ions <moshikoatia@gmail.com>
2023-11-08 13:30:33 -0800

    Write and pass 2nd test- return 'string'

--- __tests__/reverseString.test.js
+++ __tests__/reverseString.test.js
@@ -3,5 +3,9 @@ import { reverseString } from '../src/js/reverseString.js';
 describe('reverse string', () => {
   it('should be a function', () => {
     expect(typeof reverseString).toEqual('function');
-  })
-})
+  });
+  
+  it('should return a string', () => {
+    expect(typeof reverseString('hello')).toEqual('string');
+  });
+});
